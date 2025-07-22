d1bc17b77107aefea2e5852b59842595ee135832
Yago Sales <yagodsales@outlook.com.br>
2025-07-22 13:32:42 -0300

    ECR e correções gateway

--- infra/dynamo.tf
+++ infra/dynamo.tf
@@ -1,17 +1,11 @@
 resource "aws_dynamodb_table" "basic-dynamodb-table" {
   name           = var.dynamo_table
   billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "funcional"
-  range_key      = "cod_ciclo"
+  hash_key       = "id"
 
   attribute {
-    name = "funcional"
+    name = "id"
     type = "S"
-  }
-
-  attribute {
-    name = "cod_ciclo"
-    type = "N"
   }
 
   tags = {
